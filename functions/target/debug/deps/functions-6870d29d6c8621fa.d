ffc743e579d8d199198b10c6a3a7f37b4b4e44aa
Nathaniel Nanle <nathanielnanle100@gmail.com>
2024-10-21 11:02:12 +0100

    functions in Rust

---
+++ functions/target/debug/deps/functions-6870d29d6c8621fa.d
@@ -0,0 +1,5 @@
+/home/ga/Desktop/Rust.2.0/functions/target/debug/deps/functions-6870d29d6c8621fa: src/main.rs
+
+/home/ga/Desktop/Rust.2.0/functions/target/debug/deps/functions-6870d29d6c8621fa.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/ga/Desktop/Rust.2.0/functions/target/debug/functions: /home/ga/Desktop/Rust.2.0/functions/src/main.rs
